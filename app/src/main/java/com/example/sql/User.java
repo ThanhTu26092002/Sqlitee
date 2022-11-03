e5db8b87c3a6fc8744f25a052b69513c800684fd
ThanhTu26092002 <lethanhtu8102002@gmail.com>
2022-11-03 20:07:26 +0700

    upload

---
+++ app/src/main/java/com/example/sql/User.java
@@ -0,0 +1,31 @@
+package com.example.sql;
+
+public class User {
+    private int id;
+    private  String name;
+    private  String des;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+}
